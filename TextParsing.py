101f3e89d28dfba2083b9df1b2c466622efddc9f
aandime <adong@unc.edu>
2022-11-05 19:52:04 -0400

    Update

--- TextParsing.py
+++ TextParsing.py
@@ -1,22 +1,45 @@
+import datetime
 import pysrt
 import srt 
 
-id: list
-start: list
-end: list
-duration: list
-text: list
+id = 0
+start: int
+end: int
+duration: int
+content: str
+priority = 0
 
 def main():
+    """Opens the soccer video file and allows access to it's contents"""
     with open("/Users/andydong/Downloads/Soccer-Highlights/Soccermatch/Video.srt","r") as f:
         reader = srt.parse(f)
         for sub in reader:
-            id.append(sub.index)
-            start = sub.start
-            end = sub.end
-            duration = end - start
-            text = sub.content
-    print(sub)
+            start = sub.start.microseconds * 1000
+            end = sub.end.microseconds * 1000
+            duration = abs(start - end)
+            content = sub.content
+            print(start)
+            print(end)
+            print(duration)
 
+
+class HighlightClip:
+    """a class to encapsulate the metrics of a highlight clip."""
+    id: int
+    start: int
+    end: int
+    duration: int
+    text: str
+    maxDb: int
+    avgDb: int
+    priority: int
+
+    def __init__(self,id,start,end,duration,text):
+        """"""
+        self.id = id
+        self.start = start
+        self.end = end
+        self.duration = duration
+        self.text = text
 
 main()

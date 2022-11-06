0cf741a88cbd7eab9827161b671b857fb390460a
aarjav <97541934+aarjavjain2002@users.noreply.github.com>
2022-11-06 03:18:42 -0500

    Update audio.py
    Working Audio File to read intensity energy.

--- audio.py
+++ audio.py
@@ -1,77 +1,35 @@
-import librosa
-import IPython as ipd
-import matplotlib.pyplot as plt
-import numpy as np
+from HighlightClip import Clip
 import pandas as pd
+import numpy as np
+import librosa
+x, sr = librosa.load("match.wav", sr = 16000)
 
-filename = "avatar.wav"
-x, sr = librosa.load(filename, sr = 16000)
-length = librosa.get_duration(x, sr)
-print(length)
+def audioPriority(clip: Clip):
+    
+    start = clip.getStart()
+    end = clip.getEnd()
 
+    audio = x[start*sr:end*sr]
+    energy = sum(abs(audio**2))
+        
+    if energy > 750:
+      return 5
+    
+    elif energy > 600:
+      return 4
+    
+    elif energy > 400:
+      return 3
 
-# Creating slices of audio to check for audio changes
-max_slice = 5
-window_length = max_slice * sr
+    elif energy > 350:
+      return 2
 
-"""Test Audio Chunk"""
-# Audio chunk
-# a = x[21*window_length:22*window_length]
-# # ipd.display.Audio(a, rate = sr)
-
-# # Energy for the audio chunk
-# energy = sum(abs(a**2))
-# print(energy)
-
-#Visualizing the audio distribution for the chunk
-# fig = plt.figure(figsize=(14, 8))
-# ax1 = fig.add_subplot(211)
-# ax1.set_xlabel('Splice with time') 
-# ax1.set_ylabel('Amplitude') 
-# ax1.plot(a)
-# plt.show()
-"""End of Test Audio Chunk"""
-
-
-"""Full Audio Distribution"""
-# Looking at the distribution of energy
-energy = np.array([sum(abs(x[i:i+window_length]**2)) for i in range(0, len(x), window_length)])
-print(energy)
-plt.hist(energy)
-plt.show()
-
-
-
-# Defining the threshold 
-df = pd.DataFrame(columns=['energy','start','end'])
-thresh = 2700
-row_index = 0
-for i in range(len(energy)):
-  value = energy[i]
-  if(value >= thresh):
-    i = np.where(energy == value)[0]
-    print(i)
-    df.loc[row_index,'energy'] = value
-    df.loc[row_index,'start'] = i[0] * 5
-    df.loc[row_index,'end'] = (i[0]+1) * 5
-    row_index = row_index + 1
-
-# Merging consecutive time intervals into one
-temp = []
-i = 0
-j = 0
-n = len(df) - 2
-m = len(df) - 1
-while(i <= n):
-  j = i + 1
-  while(j <= m):
-    if(df['end'][i] == df['start'][j]):
-      df.loc[i,'end'] = df.loc[j,'end']
-      temp.append(j)
-      j = j + 1
+    elif energy > 150:
+      return 1
+      
     else:
-      i = j
-      break  
-df.drop(temp, axis = 0, inplace = True)
+      return 0
 
-print(df)
+
+def main():
+    return

f05f8accc7bb2b3237e6c0d1cc7d058e6d07fd09
Andy Dong <89438047+aandime@users.noreply.github.com>
2022-11-05 21:01:26 -0400

    Create TextParsing.py

---
+++ TextParsing.py
@@ -0,0 +1,27 @@
+import datetime
+import srt 
+
+id: int
+start: int
+end: int
+duration: int
+content: str
+priority = 0
+
+
+def main():
+    """Opens the soccer video file and allows access to it's contents"""
+    with open("/Users/andydong/Downloads/Soccer-Highlights/Soccermatch/Video.srt","r") as f:
+        reader = srt.parse(f)
+        for sub in reader:
+            id = sub.index
+            start = round(sub.start.seconds, 1)
+            end = round(sub.end.seconds, 1)
+            duration = round(abs(start - end))
+            content = sub.content
+            print(id, start, end, duration)
+            print(content)
+        return start, end, duration, id
+
+
+main()

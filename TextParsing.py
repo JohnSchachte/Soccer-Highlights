2bc10165c05fc874afd987dfca6c309ed7c37fe6
Yuvraj Jain <yjain@unc.edu>
2022-11-05 20:31:30 -0400

    Updated Highlight classes

--- TextParsing.py
+++ TextParsing.py
@@ -1,22 +1,58 @@
+import datetime
 import pysrt
 import srt 
 
-id: list
-start: list
-end: list
-duration: list
-text: list
+id = 0
+start: int
+end: int
+duration: int
+content: str
+priority = 0
 
 def main():
-    with open("/Users/andydong/Downloads/Soccer-Highlights/Soccermatch/Video.srt","r") as f:
+    """Opens the soccer video file and allows access to it's contents"""
+    with open("Video.srt","r") as f:
         reader = srt.parse(f)
+        i = 0
         for sub in reader:
-            id.append(sub.index)
-            start = sub.start
-            end = sub.end
-            duration = end - start
-            text = sub.content
-    print(sub)
+            print(sub)
+            start = sub.start.seconds
+            end = sub.end.seconds
+            duration = abs(start - end)
+            content = sub.content
+            # i += 1
+            # if i == 10:
+            #     break
+            print(start, end)
+        
+            
 
+class HighlightClip:
+    """a class to encapsulate the metrics of a highlight clip."""
+    id: int
+    start: int
+    end: int
+    duration: int
+    text: str
+    maxDb: int
+    avgDb: int
+    priority: int
+
+
+    def __init__(self,id,start,end,duration,text):
+        """Instantiates the class."""
+        self.id = id
+        self.start = start
+        self.end = end
+        self.duration = duration
+        self.text = text
+
+
+    def getDuration(self):
+        """Gives you the duration of the timestamp."""
+        return self.end - self.start
+
+
+    def 
 
 main()
